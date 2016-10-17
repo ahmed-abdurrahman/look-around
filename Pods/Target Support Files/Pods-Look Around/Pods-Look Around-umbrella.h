96a12e3c63c57112a525b7cdeacbc8c94da89d7c
A. Abdurrahman <ahmed.abdurrahman@begether.net>
2016-10-17 22:23:15 +0300

    Initializing cocoapods

---
+++ Pods/Target Support Files/Pods-Look Around/Pods-Look Around-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Look_AroundVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Look_AroundVersionString[];
+
---
+++ Pods/Target Support Files/Pods-Look AroundTests/Pods-Look AroundTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Look_AroundTests : NSObject
+@end
+@implementation PodsDummy_Pods_Look_AroundTests
+@end
